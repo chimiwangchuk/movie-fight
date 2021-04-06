5453b3e6161598fb8bb220ec87b42fe8e9251c89
Chimi Wangchuk <chimi.wangchuk@selise.ch>
2021-04-06 19:40:05 +0600

    calling api after certain intervals

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const fetchData = async () => {
+const fetchData = async searchTerm => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '94c19ac7',
-            s: 'avengers'
+            s: searchTerm
         }
     })
-    console.log(response.data);
 }
+let timeOutId;
+const onInput = event => {
+    if (timeOutId) {
+        clearTimeout(timeOutId);
+    }
+    timeOutId = setTimeout(() => {
+        fetchData(event.target.value);
+    }, 1000);
+    console.log(timeOutId)
+}
+const input = document.querySelector('input').addEventListener('input', onInput)
 
-fetchData();

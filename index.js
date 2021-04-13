93650484e232f3b9e0404e2cb3ca834e6a4dc0d3
Chimi Wangchuk <chimi.wangchuk@selise.ch>
2021-04-13 11:32:36 +0600

    autocomplete

--- index.js
+++ index.js
@@ -5,9 +5,41 @@ const fetchData = async searchTerm => {
             s: searchTerm
         }
     })
+    if (response.data.Error) {
+        return [];
+    }
+    return response.data.Search;
 }
-const onInput = debounce(event => {
-    fetchData(event.target.value);
-})
-const input = document.querySelector('input').addEventListener('input', onInput)
+
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+       <label> <b>Search for a movie</b></label>
+       <input type="text" class="input">
+       <div class="dropdown">
+            <div class="dropdown-menu">
+                <div class="dropdown-content results"></div>
+            </div>
+        </div>
+            `;
+
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+
+const onInput = debounce(async event => {
+    const movies = await fetchData(event.target.value);
+    resultsWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
+    for (let movie of movies) {
+        const option = document.createElement('a');
+        option.classList.add('dropdown-item');
+        option.innerHTML = `
+            <img src="${movie.Poster}" alt="poster"/>
+            ${movie.Title}
+        `;
+        resultsWrapper.appendChild(option);
+    }
+}, 500)
+input.addEventListener('input', onInput)
 
